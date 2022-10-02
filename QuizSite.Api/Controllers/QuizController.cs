82d76f0c6c0c6b8f43e76eb8e1c33063c6084303
Dwandry <Tihon.lavrov@gmail.com>
2022-10-03 00:19:10 +0300

    Nothing works

--- QuizSite.Api/Controllers/QuizController.cs
+++ QuizSite.Api/Controllers/QuizController.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using QuizSite.Domain.Database;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace QuizSite.Api.Controllers;
 
@@ -10,5 +12,12 @@ class QuizController : ControllerBase
     public QuizController(QuizDbContext dbContext)
     {
         _dbContext = dbContext;
+    }
+
+    [HttpGet("get-all-quizes")]
+    public async Task<IActionResult> getAllQuizes() {
+        var quizes = await _dbContext.Questions.ToArrayAsync();
+        System.Console.WriteLine(quizes);
+        return Ok(quizes);
     }
 }
---
+++ QuizSite.Api/wwwroot/js/index.js
@@ -0,0 +1,5 @@
+(async () => {
+    console.log("TEST");
+	const response = await fetch("get-all-quizes");	
+    console.log("response", response);
+})();
